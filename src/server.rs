d02c5fbe93ec8a210bfaa3812e41ee4cc4918e15
Ethan <eclau2003@gmail.com>
2023-12-25 20:10:24 -0500

    Initial changes

---
+++ src/server.rs
@@ -0,0 +1,21 @@
+
+#[tokio::main]
+async fn main() {
+    todo!()
+}
+
+async fn clean() {
+    todo!()
+}
+
+async fn extract_text() {
+    todo!()
+}
+
+async fn extract_boxes() {
+    todo!()
+}
+
+async fn replace() {
+    todo!()
+}
